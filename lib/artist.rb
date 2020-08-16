0b682fa0fb187fb13a3f8be26589946b5c348b3f
Saida Davis <saihdavis@gmail.com>
2020-08-16 06:21:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
   extend Memorable::ClassMethods 
   include Memorable::InstanceMethods
   extend Findable::ClassMethods
-  inlcude Paramable::InstanceMethods
+  include Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :songs
